329517372dcc803c28b3888425aac0082f67470e
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-28 22:51:48 +0600

    Втрой этап

---
+++ CustomTableView/Content.swift
@@ -0,0 +1,15 @@
+//
+//  Content.swift
+//  CustomTableView
+//
+//  Created by Shakir Kadirov on 28.01.2024.
+//
+
+import UIKit
+
+
+struct Content{
+    var image: Data
+    var title: String
+    var paragraph: String
+}
--- CustomTableView/HomePageController.swift
+++ CustomTableView/HomePageController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class HomePageController: UIViewController {
-
+    
+    let tableData: [Content] = []
+    
+    lazy var tableView: UITableView = {
+        $0.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.reuseID)
+        $0.dataSource = self
+        $0.backgroundColor = .systemBlue
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UITableView())
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Home Page"
+        navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .yellow
+        view.addSubview(tableView)
+        setConstraint()
     }
-
-
+    
+    private func setConstraint(){
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        ])
+    }
 }
 
+
+extension HomePageController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.reuseID, for: indexPath) as! CustomTableViewCell
+        return cell
+    }
+}
